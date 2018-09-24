b2587074e03c716021579ec0042681456bd006ae
Steve Edwards <sedwards2@met2dev2.turner.com>
2018-09-24 12:08:20 -0400

    committing from laptop

---
+++ app.js
@@ -0,0 +1,36 @@
+/* 
+  - Processing Amplifier (PA) Read-Only Page
+  - For use in control rooms of the CNN networks;
+    this file contains the server script for 
+    the read only PA page. 
+  - Author: Steven Edwards
+  - Page Started: September 2018
+*/
+
+// Initialize variables
+var express = require('express');
+var app = express();
+var port = 4300;
+
+app.get('/', function(req, res) {
+    res.type('text/html');
+    res.sendFile(__dirname + '/rd_only_pa_pg.html');
+    }
+);
+
+app.get('/style.css', function(req, res) {
+    res.type('text/css');
+    res.sendFile(__dirname + '/style.css');
+    }
+);
+
+app.get('/scripts/ro_script.js', function(req, res) {
+    res.type('application/javascript');
+    res.sendFile(__dirname + '/scripts/ro_script.js');
+    }
+);
+
+app.listen(port, function() {
+    console.log("My Node.js server is listening for requests for the PA READ-ONLY PAGE on port: " + port);
+   }
+);
