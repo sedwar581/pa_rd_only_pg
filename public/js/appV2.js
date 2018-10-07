744f8814ba5cb9053929f50d60741df1d4e17552
root <root@met2dev2.turner.com>
2018-10-06 22:12:36 -0400

    reconfigured file paths

--- public/js/appV2.js
+++ public/js/appV2.js
@@ -12,31 +12,33 @@ var express = require('express');
 var app = express();
 var port = 4302;
 
+app.use(express.static(__dirname + '/..'));
+
 app.get('/Read-Only-PA-Page', function(req, res) {
-    res.type('text/html');
-    res.sendFile(__dirname + '/rd_only_pa_pgV2.html');
+    res.type('html');
+    res.sendFile('/html/rd_only_pa_pgV2.html', {root: __dirname + '/..'});
 });
 
 app.get('/style.css', function(req, res) {
     res.type('text/css');
-    res.sendFile(__dirname + '/css/Green_White_papg.css');
+    res.sendFile('/css/Green_White_papg.css', {root: __dirname + '/..'});
     //res.sendFile(__dirname + '/css/Yellow_Green_papg.css');
     //res.sendFile(__dirname + '/css/White_Green_papg.css');
 });
 
 app.get('/graphics/VALID_TS', function(req, res) {
     res.type('image/gif');
-    res.sendFile(__dirname + '/graphics/VALID_TS_loop_23fps_480px.gif');
+    res.sendFile('/graphics/VALID_TS_loop_23fps_480px.gif', {root: __dirname + '/..'});
 });
 
-app.get('/scripts/ro_scriptV2.js', function(req, res) {
+app.get('/ro_scriptV2.js', function(req, res) {
     res.type('application/javascript');
-    res.sendFile(__dirname + '/scripts/ro_scriptV2.js');
+    res.sendFile('/ro_scriptV2.js', {root: __dirname});
 });
 
 app.get('/pa_data', function(req, res) {
     res.type('text/plain');
-    res.sendFile(__dirname + '/quartz_res.txt');
+    res.sendFile('/data/quartz_res.txt', {root: __dirname + '/..'});
 });
 
 app.listen(port, function() {
